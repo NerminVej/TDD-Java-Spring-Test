69d4525b8e677574bc36b94c2648363a77c398a1
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-21 09:00:01 +0100

    created the post record

---
+++ src/main/java/com/example/TDDTest/post/Post.java
@@ -0,0 +1,9 @@
+package com.example.TDDTest.post;
+
+public record Post(
+        Integer id,
+        Integer userId,
+        String title,
+        String body
+) {
+}
--- src/test/java/com/example/TDDTest/post/PostControllerTest.java
+++ src/test/java/com/example/TDDTest/post/PostControllerTest.java
@@ -1,4 +1,9 @@
 package com.example.TDDTest.post;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(PostController.class)
+@AutoConfigureMockMvc
 public class PostControllerTest {
 }
