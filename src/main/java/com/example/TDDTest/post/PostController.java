fdaed958ad5b1ebfb4ab78c49206f7595705bf00
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-22 16:06:05 +0100

    added the postcontroller postmapping

--- src/main/java/com/example/TDDTest/post/PostController.java
+++ src/main/java/com/example/TDDTest/post/PostController.java
@@ -2,10 +2,8 @@ package com.example.TDDTest.post;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +29,12 @@ public class PostController {
     @GetMapping("/{id}")
     Optional<Post> findById(@PathVariable Integer id) {
         return Optional.ofNullable(repository.findById(id).orElseThrow(PostNotFoundException::new));
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    Post save(@RequestBody @Valid Post post) {
+        return repository.save(post);
     }
 
 }
