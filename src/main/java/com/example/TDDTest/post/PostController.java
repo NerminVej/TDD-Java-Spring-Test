930bc2f3e492721ff92d37b7fe204c011de2758e
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-22 09:04:12 +0100

    created a simple get endpoint

--- src/main/java/com/example/TDDTest/post/PostController.java
+++ src/main/java/com/example/TDDTest/post/PostController.java
@@ -1,4 +1,20 @@
 package com.example.TDDTest.post;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
+
+
+    @GetMapping("")
+    List<Post> findAll() {
+        return null;
+    }
+
+
 }
