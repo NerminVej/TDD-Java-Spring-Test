df0548e00a404df37640e0cf4badaf34a7ba4855
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-22 08:57:23 +0100

    created the setup for the test

--- src/test/java/com/example/TDDTest/post/PostControllerTest.java
+++ src/test/java/com/example/TDDTest/post/PostControllerTest.java
@@ -1,9 +1,34 @@
 package com.example.TDDTest.post;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @WebMvcTest(PostController.class)
 @AutoConfigureMockMvc
 public class PostControllerTest {
+
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+
+    List<Post> posts = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        posts = List.of(
+                new Post(1,1,"Hello, World!", "This is my first post.",null),
+                new Post(2,1,"Second Post", "This is my second post.",null)
+        );
+    }
+
+
 }
