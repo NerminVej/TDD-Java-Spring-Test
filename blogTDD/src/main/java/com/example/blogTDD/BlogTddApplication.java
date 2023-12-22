7609bb75a36cae6656e6cb099b189e1ae13ff0e6
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-22 20:56:57 +0100

    created the blogTDD project

---
+++ blogTDD/src/main/java/com/example/blogTDD/BlogTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogTDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTddApplication.class, args);
+	}
+
+}
---
+++ blogTDD/src/test/java/com/example/blogTDD/BlogTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogTDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
