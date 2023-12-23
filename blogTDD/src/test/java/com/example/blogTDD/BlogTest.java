9a015c12a8356f5b1557f39fce7e2f3d98d42612
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-23 16:05:32 +0100

    created the blog entity and made our first test pass

--- blogTDD/src/test/java/com/example/blogTDD/BlogTest.java
+++ blogTDD/src/test/java/com/example/blogTDD/BlogTest.java
@@ -1,6 +1,10 @@
 package com.example.blogTDD;
 
+import com.example.blogTDD.models.Blog;
 import org.junit.jupiter.api.Test;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BlogTest {
 
