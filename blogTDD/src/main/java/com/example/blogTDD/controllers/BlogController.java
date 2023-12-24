c42b3ac61ecf7b59ed11d0851d6145c3961db638
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-24 11:10:37 +0100

    created the getmapping to get a blog by id inside of the controller

--- blogTDD/src/main/java/com/example/blogTDD/controllers/BlogController.java
+++ blogTDD/src/main/java/com/example/blogTDD/controllers/BlogController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/blogs")
@@ -23,6 +25,14 @@ public class BlogController {
         List<Blog> blogs = blogService.getAllBlogs();
         return new ResponseEntity<>(blogs, HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Blog> getBlogById(@PathVariable Long id) {
+        Optional<Blog> blog = blogService.getBlogById(id);
+        return blog.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
 
 
 }
