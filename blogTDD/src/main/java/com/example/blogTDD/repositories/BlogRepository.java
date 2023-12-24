4149e6a7d94dd92a426114b6786da6cccc4016b0
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-24 09:17:50 +0100

    forgot to add the h2 embedded database for testing puproses

--- blogTDD/src/main/java/com/example/blogTDD/repositories/BlogRepository.java
+++ blogTDD/src/main/java/com/example/blogTDD/repositories/BlogRepository.java
@@ -2,7 +2,8 @@ package com.example.blogTDD.repositories;
 
 import com.example.blogTDD.models.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface BlogRepository extends JpaRepository<Blog, Long> {
 }
--- blogTDD/src/test/java/com/example/blogTDD/BlogRepositoryTest.java
+++ blogTDD/src/test/java/com/example/blogTDD/BlogRepositoryTest.java
@@ -3,13 +3,16 @@ package com.example.blogTDD;
 import com.example.blogTDD.models.Blog;
 import com.example.blogTDD.repositories.BlogRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DataJdbcTest
+@DataJpaTest
 public class BlogRepositoryTest {
 
     @Autowired
