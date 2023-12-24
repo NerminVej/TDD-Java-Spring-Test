bd4cd26b71b428de6dc3ada2114e282f41e316e2
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-24 11:11:34 +0100

    created the createblog method inside of the controller

--- blogTDD/src/main/java/com/example/blogTDD/controllers/BlogController.java
+++ blogTDD/src/main/java/com/example/blogTDD/controllers/BlogController.java
@@ -5,10 +5,7 @@ import com.example.blogTDD.services.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +28,12 @@ public class BlogController {
         Optional<Blog> blog = blogService.getBlogById(id);
         return blog.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Blog> createBlog(@RequestBody Blog blog) {
+        Blog createdBlog = blogService.createBlog(blog);
+        return new ResponseEntity<>(createdBlog, HttpStatus.CREATED);
     }
 
 
